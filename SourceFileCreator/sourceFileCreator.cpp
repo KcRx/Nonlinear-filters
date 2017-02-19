f89db6185e18e6ddc65a1fcf54d5cfbeb46b26a7
Tomek <kaczmaretomasz@rocketmail.com>
2017-02-19 15:35:53 +0100

    quick Bugfixing + output file with description

--- SourceFileCreator/sourceFileCreator.cpp
+++ SourceFileCreator/sourceFileCreator.cpp
@@ -19,10 +19,10 @@
 /*
  * 
  */
-void addPreambule( std::complex<float> *complex, int size, int sign){
+void addPreambule( std::complex<float> *complex, int size, int sign){    
     for(int index = 0; index < size; index++, sign++){       
         if(index % 8 == 4){
-            complex[sign] = {1,0};
+            complex[sign] = {1,1};
         }else{
             complex[sign] = {0,0};
         }
@@ -35,6 +35,24 @@ void addPayload( std::complex<float> *complex, int size, int sign){
         int imag = std::rand() % 2;
         complex[sign] = {real,imag};
     }
+}
+
+void showPreambule(std::complex<float> *complex, int frame, int size, int sign){
+    std::cout << "FRAME #" << frame << "\n";
+    std::cout << "###########################" << "\n";
+    std::cout << "Preambule" << "\n";
+    for(int index = 0; index < size; index++, sign++){
+        std::cout << complex[sign] << " ";
+    }
+    std::cout << "\n" << "###########################" << "\n";
+}
+
+void showPayload( std::complex<float> *complex, int frame, int size, int sign){
+    std::cout << "Payload" << "\n";
+    for(int index = 0; index < size; index++, sign++){
+        std::cout << complex[sign] << " ";
+    }
+    std::cout << "\n" << "###########################" << "\n" << "\n";
 }
 
 int main(int argc, char* argv){ 
@@ -54,5 +72,14 @@ int main(int argc, char* argv){
     }
         
     std::ofstream stream("source.bin", std::ios::binary);    
-    stream.write(reinterpret_cast<const char*>(&complex), sizeof complex);   
+    stream.write(reinterpret_cast<const char*>(&complex), sizeof complex);
+ /*   
+    sign=0;
+     for(int frame = 0; frame < numberOfFrames; frame++){
+         showPreambule(complex, frame, preambuleSize, sign);
+         sign += preambuleSize;
+         showPayload(complex, frame, payloadSize, sign);
+         sign += payloadSize;
+     }     
+  * /
 }
